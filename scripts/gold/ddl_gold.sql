5f942cc82b98949fee8e1ce008ba1af82480552b
Amani Mulira <67328747+amanimulira@users.noreply.github.com>
2025-09-21 11:37:12 +0100

    Update ddl_gold.sql
    gold.facts_sales view updates

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -76,7 +76,7 @@ SELECT
     pr.product_key AS product_key, 
     cu.customer_key AS customer_key, 
     sd.sls_order_dt AS order_date, 
-    sd.sls_ship_dy AS shipping_date, 
+    sd.sls_ship_dt AS shipping_date, 
     sd.sls_due_dt AS due_date, 
     sd.sls_sales AS sales_amount, 
     sd.sls_quantity AS quantity,
@@ -84,6 +84,6 @@ SELECT
 FROM silver.crm_sales_details sd
 LEFT JOIN gold.dim_products pr
     ON sd.sls_prd_key = pr.product_number
-LEFT JOIN gold.dim_cusotmers cu
+LEFT JOIN gold.dim_customers cu
     ON sd.sls_cust_id = cu.customer_id;
 GO
